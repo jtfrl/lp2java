8658e72c3a60aa11f0faf1ca440aa64378f1610b
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-08-14 20:21:06 +0000

    add prob on declaring vectors

---
+++ introjava/Introjava.java
@@ -0,0 +1,25 @@
+class Introjava{
+    public static void main(String args[]){
+        System.out.println("Olá\n");
+
+        String[] v=new String [5];
+        v[0]="+";
+
+       // v={"*", "³", "Ö", "a", "Ø"};
+       
+
+        int[] t=new int[5];
+
+        t[]={4,5,99,12,1};
+
+        /*
+        outras implementações com vetor
+        int[][]s= new int[3][];
+
+        s[3][]
+
+        //checar depois
+
+        */
+    }
+}
