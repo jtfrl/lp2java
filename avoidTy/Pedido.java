dddf8d81d6b455ca1fcd1d54436c37956e02d7e9
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-04 21:00:17 +0000

    add; still to be impl

---
+++ avoidTy/Pedido.java
@@ -0,0 +1,18 @@
+public class Pedido{
+    statusPedido sts;
+    double valor;
+    String nomePrato; //poderia ser um 
+
+    //Pedido() >>> construtor pra criar pedido
+    //voidModPed() >>> muda o pedido
+    // seeStatus() >>>  void para apenas mostrar o sttus
+
+
+    /*
+    Antes de tentar entregar um pedido, o sistema deve 
+    verificar se ele está pronto para ser 
+    
+    entregue. Somente pedidos que estejam "prontos" poderão ser entregues.
+
+    */
+}
---
+++ avoidTy/statusPedido.java
@@ -0,0 +1,9 @@
+public enum statusPedido{
+    PENDENTE,
+    PREPARANDO,
+    PRONTO, 
+    ENTREGUE
+
+    //Um pedido começa como "Pendente", 
+    //depois vai para "Preparando", "Pronto" e "Entregue".
+}
