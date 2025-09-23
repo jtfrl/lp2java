06d857531c830d4c355de128112703ff5fafdd7e
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-23 22:18:17 +0000

    add

--- workerAdd/main.java
+++ workerAdd/main.java
@@ -1,5 +1,15 @@
 public class main{
     public static void main(String[] args) {
+        wID[] numID=new wID[3];
+        //numID[0]=19111919;
+
+
+        Worker w1=new Worker("José", numID[0], 512.20);
+        Worker w2=new Worker("Rodrigo", 19113819, 1512.20);
+        Worker w3=new Worker("Alessandra", 19115719, 2512.20);
+
+
+
         
     }
 }
