de8dfd3d954a6d61b64a134cd288bdeb04f42a08
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-09-23 20:58:04 +0000

    still to be impl

---
+++ borrowBookCol/LivroBib.java
@@ -0,0 +1,54 @@
+
+/*
+- Um usuário só pode emprestar livros se 
+tiver menos de 3 livros emprestados.
+
+- Um livro só pode ser emprestado se 
+estiver disponível.
+*/
+
+class LivroBib{
+    private int cod, qtd;
+     private String titulo, autor;
+    private Boolean regDev; //registro de devolução
+     private LivroBib prox;
+
+    public LivroBib(int c, int q, String t, String a){
+        this.cod=c;
+        this.qtd=q;
+        this.titulo=t;
+        this.autor=a;
+        this.prox=null;
+    }
+
+    boolean devolver(){ 
+        this.qtd++;
+        System.out.println("Devolução realizada: "+this.titulo);
+        return regDev=true;
+    }
+
+    boolean posRealizarEmp(){
+         //mostra se o livro saiu com emprestimo realizado
+        //colocado como para Emprestimo, mas ta aqui 
+        if(this.qtd>0){ 
+            this.qtd--; 
+            System.out.println("Empréstimo realizado.\n"); 
+            return regDev=true;
+        }
+        else{
+            System.out.println("Livro não disponível\n");
+            return regDev=false;
+        }
+    }   
+
+
+//Integer qtdEmp=lEmp.qtd;
+//lista_l.stream().filter( n-> n<3).<.>
+/*
+
+no lugar de <.> colocar a implementação restante
+*/
+
+
+
+}
